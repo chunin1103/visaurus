185aaeb06a00dda6496a85a885d91adb7b913b5b
Tuan Anh <chunin1103@gmail.com>
2022-09-10 10:32:20 +0700

    get all syms

--- crawl_data_thesaurus_official.py
+++ crawl_data_thesaurus_official.py
@@ -1,27 +1,34 @@
 from urllib.request import urlopen
-from word import key_word
+from bs4 import BeautifulSoup
 # import os
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"E:\Projects\.new_chapter_for_thesaurus\Credentials\vi-thesaurus-c8077aebe0c3.json"
 
-ini_url = "https://www.thesaurus.com/browse/"
+def crawlThesaurus(word):
+    # Url crawl
+    ini_url = "https://www.thesaurus.com/browse/"
+    url = ini_url + word
+    try:
+        # bs4 connect 
+        conn = urlopen(url)
+        urlopen(url)
+        raw_data = conn.read()
+        webpage_text = raw_data.decode("utf-8")
 
-url = ini_url + "eat"
-from bs4 import BeautifulSoup
+        # Crawl all page with soup
+        soup = BeautifulSoup(webpage_text, "html.parser")
 
+        # Get all words
+        section = soup.find("section", {'class': "css-1fzg0m e1v16r9g0"})
+        ul      = section.find("div", {'class': "css-1fsijta eebb9dz0"})
+        li_list = ul.find_all("li")
 
-conn = urlopen(url)
-urlopen(url)
-raw_data = conn.read()
-webpage_text = raw_data.decode("utf-8")
-soup = BeautifulSoup(webpage_text, "html.parser")
+        symnonym_list = []
+        for __ in li_list:
+            global word_crawled
+            word_crawled = __.text.strip()
+            symnonym_list.append(word_crawled)
+        return symnonym_list, word_crawled
+    except:
+        pass 
 
-section = soup.find("section", {'class': "css-1fzg0m e1v16r9g0"})
-ul      = section.find("div", {'class': "css-1fsijta eebb9dz0"})
-li_list = ul.find_all("li")
-
-symnonym_list = []
-for __ in li_list:
-    word_crawled = __.text.strip()
-    symnonym_list.append(word_crawled)
-
-print(symnonym_list)    
+crawlThesaurus('rebel')
