be76df375004441f0882287be2aa475adac99f94
TA2 <hi.its.tuananh@gmail.com>
2022-09-15 02:29:17 +0700

    onelook

--- crawl_data_onelook_official.py
+++ crawl_data_onelook_official.py
@@ -1,21 +1,31 @@
 import requests
 import collections
-from word import key_word
 
-ini_url = "https://api.onelook.com/words?v=ol_gte3&ml=%20{{ u }}&qe=ml&md=dp&max=1000&k=olthes_r4"
-url = ini_url.replace("{{ u }}", key_word)
-r = requests.get(url)
-data = r.json()
+def crawlOnelook(word):
+    try:
+        word = word.replace(' ', "+")
+        word = word.replace('!', '')
+        word = word.replace('?', '')
+        word = word.strip()
+        ini_url = "https://api.onelook.com/words?v=ol_gte3&ml=%20{{ u }}&qe=ml&md=dp&max=1000&k=olthes_r4"
+        url = ini_url.replace("{{ u }}", word)
+        r = requests.get(url)
+        data = r.json()
 
-tu_dong_nghia =[]
-# for _ in range (0, 25):
-#     if data[_]['word'] is not None:
-#         tu_dong_nghia.append(data[_]['word'])
-n = 0
-for i in data:
-    n += 1
-    if i['word'] is not None and n <= 100:
-        i['word'] = i['word'].strip()
-        tu_dong_nghia.append(i['word'])
-    
-print(tu_dong_nghia)
+        global tu_dong_nghia
+        tu_dong_nghia =[]
+        # for _ in range (0, 25):
+        #     if data[_]['word'] is not None:
+        #         tu_dong_nghia.append(data[_]['word'])
+        n = 0
+        for i in data:
+            n += 1
+            if i['word'] is not None and n <= 100:
+                i['word'] = i['word'].strip()
+                tu_dong_nghia.append(i['word'])
+        return tu_dong_nghia
+    except TypeError as e:
+        print(e)
+        print("handled successfully")
+    print(tu_dong_nghia)
+
---
+++ get_symnonym_onelook.py
@@ -0,0 +1,38 @@
+import csv
+from encodings import utf_8
+from crawl_data_onelook_official import *
+
+# open the file in the write mode
+f = open('symnonyms_eng_onelook.csv', 'w', encoding="utf-8", newline='')
+
+# create the csv writer
+writer = csv.writer(f)
+
+
+eng_word_list =[]
+with open('all30k-Vitoen.csv', encoding='utf_8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        eng_word_list.append(row['a'])
+
+eng_sym_list =[]
+
+for eng_word in eng_word_list:
+    print(eng_word)
+    eng_word = eng_word.replace("a ", "")
+    eng_word = eng_word.replace("A ", "")
+    eng_word = eng_word.replace("the ", "")
+    eng_word = eng_word.replace("The ", "")
+    try:
+        symnonym_word = crawlOnelook(eng_word)
+    except:
+        symnonym_word = ["cannot found"]
+    eng_word = [eng_word]
+    eng_word.append(symnonym_word)
+    eng_sym_list.append(eng_word)
+    print(eng_sym_list)
+
+for i in eng_sym_list:
+    writer.writerow(i)
+
+f.close()
