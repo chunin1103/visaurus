234b2e277e49013149d4a557c6bf74c2cd68dd51
TA2 <hi.its.tuananh@gmail.com>
2022-09-19 09:17:54 +0700

    yes

--- translate_symnonym.py
+++ translate_symnonym.py
@@ -4,14 +4,14 @@ import csv
 from encodings import utf_8
 
 # open the file in the write mode
-f = open('symnonyms_vi.csv', 'w', encoding="utf-8", newline='')
+f = open('symnonyms_vi_onelook.csv', 'w', encoding="utf-8", newline='')
 # create the csv writer
 writer = csv.writer(f)
 
 sym_list =[]
-with open('Symnonym_List.csv', encoding='utf8') as csvfile:
+with open('symnonyms_eng_onelook_ver1.csv', encoding='utf8') as csvfile:
     reader = csv.DictReader(csvfile)
-    # structure: [{"Word": "abcd", "Symnonym": ["abc", "def"]}]
+    # structure: [{"Word": "abcd", "Symnonyms": ["abc", "def"]}]
     for i in reader:
         tu_dn = i["Symnonyms"]
         tu_dn = [tu_dn]
