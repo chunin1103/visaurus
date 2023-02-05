054556dcd3652c4d16d3624b3b3a0a2cd84f5b14
chunin1103 <chunin1103@gmail.com>
2023-02-05 14:37:04 +0700

    web viable

--- find_more_vie.py
+++ find_more_vie.py
@@ -1,24 +1,25 @@
 import pandas as pd
 
-# Create data frame
-df = pd.DataFrame({
-    'Vietnamese Word': ['Tên', 'số', 'sức', 'mạnh', 'người'],
-    'Synonyms': ['tên đệm', 'chữ số', 'sức lực', 'cường độ', 'con người']
-})
+# Declare all columns
+col_list = ["Từ gốc","Từ dịch", "In hoa?","Symnonym","Từ đồng nghĩa","Symnonym_Onelook", "Từ đồng nghĩa Onelook", "Globse", "Thesaurus.com", "Onelook", "merged"]
+df = pd.read_csv("82k_vi_merged.csv", encoding="utf8", usecols=col_list)
+
+# Convert the columns to string type
+df['Từ gốc'] = df['Từ gốc'].astype(str)
+df['merged'] = df['merged'].astype(str)
 
 # Create a third column to store additional words
-df['New Words'] = None
+df['New_Words'] = None
 
-# Loop through the words in column 2
-for i, synonym in enumerate(df['Synonyms']):
-    # Split the words in column 2
-    syn_words = synonym.split(',')
-    # Loop through the split words
-    for word in syn_words:
-        # Check if the word is in column 1
-        if word not in list(df['Vietnamese Word']):
-            # If not, add the word to the third column
-            df.at[i, 'New Words'] = word
+merged_words = set(word.strip() for words in df["merged"].str.strip().str.split(',') for word in words)
+original_words = set(df["Từ gốc"].str.strip())
+new_words = merged_words.difference(original_words)
 
-# Print the data frame
-print(df)
+new_words = list(new_words)
+import pandas as pd
+
+# Convert the list of words to a DataFrame
+df = pd.DataFrame({'words': new_words})
+
+# Export the DataFrame to an Excel file
+df.to_excel('words2.xlsx', index=False)
---
+++ webpage/my_flask_app/app.py
@@ -0,0 +1,42 @@
+import sqlite3
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+def get_synonyms(word):
+    conn = sqlite3.connect("synonyms.db")
+    cur = conn.cursor()
+    cur.execute("SELECT synonyms FROM symnonyms WHERE word=?", (word,))
+    result = cur.fetchone()
+    conn.close()
+    return result[0] if result else "Word not found."
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+# def search():
+#     if request.method == 'POST':
+#         word = request.form['word']
+#         con = sqlite3.connect('synonyms.db')
+#         cur = con.cursor()
+#         query = "SELECT synonyms FROM symnonyms WHERE word='{}'".format(word)
+#         cur.execute(query)
+#         result = cur.fetchone()
+#         con.close()
+#         if result:
+#             return result[0]
+#         else:
+#             return "Word not found"
+#     return render_template('search.html')
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        word = request.form["word"]
+        synonyms = get_synonyms(word)
+        return render_template("search2.html", word=word, synonyms=synonyms)
+    return render_template("search2.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
