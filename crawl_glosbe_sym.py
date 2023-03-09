432e5813427db3c8fd3e6dd34534e494a7bbbd6b
chunin1103 <chunin1103@gmail.com>
2023-03-10 01:53:25 +0700

    glbe

--- crawl_glosbe_sym.py
+++ crawl_glosbe_sym.py
@@ -18,27 +18,41 @@ def get_glosbe(word):
 
     webpage_text = raw_data.decode("utf-8")
     soup = BeautifulSoup(webpage_text, "html.parser")
-    div1        = soup.find("div",{'class': 'phrase__translation__section'})
-    div2        = div1.find_next("ul",{'class': 'translations__list'})
-    all_li      = div2.find_all("li", {'class': 'translation__item'})
-    sym_list = []
-    
-    for li in all_li:
-        all_span    = li.find_all('span', {'class': 'translation__item__phrase'})
-        for span in all_span:
-            span = span.text
-            span = span.strip()
-            sym_list.append(span)
+    div1        = soup.find("main",{'id': 'dictionary-content'})
+    div2        = div1.find_next("section",{'class': 'bg-white px-1'})
+    div3        = div2.find_next("div",{"class": "pl-1"})
+    div4        = div3.find_next("ul",{"class": "pr-1"})
+    all_li      = div4.find_all("li")
 
-    # less frequent
-    try:
-        div3        = soup.find("div", {'class', 'px-2 text-sm font-medium less-frequent-translations__list-compact'})
-        for span in div3:
-            span = span.text
-            span = span.strip()
-            if span != "·":
-                sym_list.append(span)
-    except:
-        pass
+    sym_list = []
+
+    for i in all_li:
+        i = i.find_next("h3")
+        i = i.text
+        sym_list.append(i)
     print(sym_list)
     return sym_list
+
+
+    # all_li      = div2.find_all("li", {'class': 'translation__item'})
+    # sym_list = []
+    
+    # for li in all_li:
+    #     all_span    = li.find_all('span', {'class': 'translation__item__phrase'})
+    #     for span in all_span:
+    #         span = span.text
+    #         span = span.strip()
+    #         sym_list.append(span)
+
+    # # less frequent
+    # try:
+    #     div3        = soup.find("div", {'class', 'px-2 text-sm font-medium less-frequent-translations__list-compact'})
+    #     for span in div3:
+    #         span = span.text
+    #         span = span.strip()
+    #         if span != "·":
+    #             sym_list.append(span)
+    # except:
+    #     pass
+    # print(sym_list)
+    # return sym_list
--- get_glosbe.py
+++ get_glosbe.py
@@ -10,24 +10,24 @@ writer = csv.writer(f)
 
 
 eng_word_list =[]
-with open('all30k-Vitoen.csv', encoding='utf_8') as csvfile:
+with open('all30k.csv', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        eng_word_list.append(row['a'])
+        eng_word_list.append(row['tu_tieng_Anh'])
 
 eng_sym_list =[]
 n=0
 m=0
 for eng_word in eng_word_list:
     n+=1
-    if 23000<n<24000:
-        print(eng_word)
+    if n<12:
         eng_word = eng_word.replace("a ", "")
         eng_word = eng_word.replace("A ", "")
         eng_word = eng_word.replace("the ", "")
         eng_word = eng_word.replace("The ", "")
         try:
             symnonym_word = get_glosbe(eng_word)
+        # to handle sometimes calling gets blocked
         except:
             symnonym_word = ["cannot found"]
             m += 1
@@ -35,6 +35,7 @@ for eng_word in eng_word_list:
                 wait = input("crashed, press Enter to continue.")
         else:
             m = 0
+        print(eng_word)
         eng_word = [eng_word]
         eng_word.append(symnonym_word)
         eng_sym_list.append(eng_word)
